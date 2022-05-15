1e891c3ddd7dad1b0fa8dcc4fd5e415d46256886
stepantsuk <stepan.tsukanov@yandex.ru>
2022-05-15 14:57:44 +0300

    vertical accordeon

--- js/main.js
+++ js/main.js
@@ -10,3 +10,30 @@ let toggleMenu = (e) => {
 };
 
 icon.addEventListener("click", toggleMenu);
+
+/// вертикальный аккордеон
+
+let member = document.querySelectorAll(".member__link");
+
+const verticalAccordeon = () => {
+  member.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      let activeItem = document.querySelector(".member--active");
+      if (activeItem) {
+        let memberReview = activeItem.querySelector(".member__review");
+        console.log(memberReview);
+        memberReview.style.height = 0 + "px";
+        activeItem.classList.remove("member--active")
+      }
+      if (!activeItem || e.target !== activeItem.querySelector(".member__link")) {
+        let currentMember = e.target.closest(".member");
+        currentMember.classList.add("member--active");
+
+        currentMember.querySelector(".member__review").style.height = currentMember.querySelector(".member__review").scrollHeight + 'px';
+      }
+    })
+  })
+}
+
+verticalAccordeon();
